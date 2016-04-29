ae8fc64a1326208d354498104d7735383289e1cc
Jalen Adams <leftysolara@gmail.com>
2016-04-29 18:50:21 -0400

    add license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,26 @@
+/******************************************************************************
+ * main.cpp : entry point for the application
+ * ****************************************************************************
+ * Copyright (C) 2016 Jalen Adams
+ *
+ * Authors: Jalen Adams <jalenkadams@gmail.com>
+ *
+ * This file is part of Bookmarks.
+ *
+ * Bookmarks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bookmarks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bookmarks. If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
 #include "mainwindow.h"
 #include <QApplication>
 
