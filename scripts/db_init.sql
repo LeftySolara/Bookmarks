121d5a9a6f7dd7f983ae8634e9bbb45f98c306aa
Jalen Adams <leftysolara@gmail.com>
2016-05-02 09:04:54 -0400

    add manga and sources tables to sql script

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -1,20 +1,47 @@
 --
--- File generated with SQLiteStudio v3.0.7 on Fri Apr 29 19:14:16 2016
+-- File generated with SQLiteStudio v3.0.7 on Mon May 2 09:02:37 2016
 --
 -- Text encoding used: UTF-8
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Table: Show
-DROP TABLE IF EXISTS Show;
+-- Table: show
+DROP TABLE IF EXISTS show;
 
-CREATE TABLE Show (
-    name       TEXT PRIMARY KEY ON CONFLICT ROLLBACK
-                    NOT NULL,
-    ep_watched INT  DEFAULT (0),
-    ep_total   INT  NOT NULL,
-    art_path   TEXT
+CREATE TABLE show (
+    title      TEXT    PRIMARY KEY ON CONFLICT ROLLBACK
+                       NOT NULL,
+    ep_watched INT     DEFAULT (0),
+    ep_total   INT     NOT NULL,
+    art_path   TEXT,
+    ongoing    BOOLEAN NOT NULL
+                       DEFAULT (0) 
+);
+
+
+-- Table: manga
+DROP TABLE IF EXISTS manga;
+
+CREATE TABLE manga (
+    title          TEXT    PRIMARY KEY
+                           NOT NULL,
+    chapters_read  INT     DEFAULT (0),
+    chapters_total INT     NOT NULL,
+    art_path       TEXT,
+    ongoing        BOOLEAN NOT NULL
+                           DEFAULT (0) 
+);
+
+
+-- Table: sources
+DROP TABLE IF EXISTS sources;
+
+CREATE TABLE sources (
+    name TEXT PRIMARY KEY
+              UNIQUE
+              NOT NULL,
+    link TEXT
 );
 
 
